66f82c80a054e34c132bb6a5e94d6fc344ebbeda
Rulio Souza <ruliosouzaa@gmail.com>
2025-08-17 06:15:36 +0000

    pode diminuir o tamnho do botao esta gigante e desproporcional

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -505,7 +505,7 @@ export default function HomePage() {
                       </Link>
                     </SheetClose>
                     <SheetClose asChild>
-                        <Button variant="destructive" className="w-full justify-start gap-4 p-2 text-base font-semibold" onClick={handleSignOut}>
+                        <Button variant="destructive" className="w-full justify-start gap-4 py-2 font-semibold" onClick={handleSignOut}>
                             <LogOut className="h-6 w-6" />
                             Sair
                         </Button>
