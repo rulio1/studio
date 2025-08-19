687bac365c2766f18ac7adb91c60aec5f7ef6066
Rulio Souza <ruliosouzaa@gmail.com>
2025-08-19 16:38:56 +0000

    tem que subir um pouco o footer no desktop pra nao ter rolagem

--- src/components/landing/desktop-landing.tsx
+++ src/components/landing/desktop-landing.tsx
@@ -86,7 +86,7 @@ export default function DesktopLanding() {
           </div>
         </section>
       </main>
-      <footer className="w-full p-4 text-center mt-auto flex flex-col items-center">
+      <footer className="w-full p-4 text-center flex flex-col items-center">
         <p className="text-xs text-muted-foreground">
           &copy; 2025 Zispr Inc. Todos os direitos reservados.
           <br />
