e66b31b9adfc71deb0337f6e2a1176f7133b2143
Rulio Souza <ruliosouzaa@gmail.com>
2025-08-19 17:23:51 +0000

    remova o fivecon do firebase e substitua pelo meu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,6 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'Zispr',
   description: 'O melhor lugar para se conectar com o mundo. Participe da conversa.',
-  icons: {
-    icon: '/icon.svg',
-    shortcut: '/icon.svg',
-    apple: '/icon.svg',
-  }
 };
 
 export default function RootLayout({
@@ -21,6 +16,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
