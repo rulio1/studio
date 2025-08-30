f67e8f7d5bee29398c4501aafb6c04ad6e0495ea
Rulio Souza <ruliosouzaa@gmail.com>
2025-08-30 02:12:54 +0000

    ao clicar aparece isso

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,13 @@
 
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables.');
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2024-06-20',
   typescript: true,
 });
