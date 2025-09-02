aa75bbc701a447317fd41ed2e0a0c7db8d4144e2
Rulio Souza <ruliosouzaa@gmail.com>
2025-09-02 07:00:58 +0000

    vamos la

--- src/lib/stripe/server.ts
+++ src/lib/stripe/server.ts
@@ -1,13 +1,1 @@
-
-import Stripe from 'stripe';
-
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
-
-if (!stripeSecretKey) {
-  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables.');
-}
-
-export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2024-06-20',
-  typescript: true,
-});
+// This file has been intentionally left blank to remove Stripe functionality.
