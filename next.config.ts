4e8ca0095424fc76e741ddce4d331da8db6e1f73
Rulio Souza <ruliosouzaa@gmail.com>
2025-09-09 14:57:14 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -210,6 +210,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'ds-images.bolavip.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'p2.trrsf.com',
+      },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
