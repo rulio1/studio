e064b310693838815cc4ebf46bdb128e9e012dea
Rulio Souza <ruliosouzaa@gmail.com>
2025-09-15 03:03:10 +0000

    Try fixing this error: `Build Error: Export uploadImageAndGetURL doesn't

--- src/components/create-post-modal.tsx
+++ src/components/create-post-modal.tsx
@@ -6,7 +6,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter, DialogC
 import { Textarea } from './ui/textarea';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { useToast } from '@/hooks/use-toast';
-import { auth, db } from '@/lib/firebase';
+import { auth, db, storage } from '@/lib/firebase';
 import { collection, doc, onSnapshot, runTransaction, serverTimestamp, writeBatch, addDoc, query, where, limit, getDocs } from 'firebase/firestore';
 import { User as FirebaseUser, onAuthStateChanged } from 'firebase/auth';
 import { Loader2, X, ImageIcon, ListOrdered, Smile, MapPin, Globe, Users, AtSign, BadgeCheck, Bird, Sparkles } from 'lucide-react';
@@ -24,7 +24,8 @@ import { generatePost } from '@/ai/flows/post-generator-flow';
 import { generateImageFromPrompt } from '@/ai/flows/image-generator-flow';
 import { useTranslation } from '@/hooks/use-translation';
 import GifPicker from './gif-picker';
-import { uploadImageAndGetURL } from '@/actions/storage';
+import { ref as storageRef, uploadString, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
 
 
 interface Post {
@@ -331,7 +332,10 @@ export default function CreatePostModal({ open, onOpenChange, quotedPost }: Crea
         try {
             let imageUrl: string | null = null;
             if (postImageDataUri) {
-                imageUrl = await uploadImageAndGetURL(postImageDataUri, user.uid);
+                const fileType = postImageDataUri.split(';')[0].split('/')[1];
+                const imageRef = storageRef(storage, `${user.uid}/posts/${uuidv4()}.${fileType}`);
+                const uploadTask = await uploadString(imageRef, postImageDataUri, 'data_url');
+                imageUrl = await getDownloadURL(uploadTask.ref);
             }
 
             const hashtags = extractHashtags(newPostContent);
@@ -772,3 +776,5 @@ export default function CreatePostModal({ open, onOpenChange, quotedPost }: Crea
         </Dialog>
     );
 }
+
+    
